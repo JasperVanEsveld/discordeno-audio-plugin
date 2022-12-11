c690c2aad166fad4dd0c49befc8bd9c7663bdf33
Jasper van Esveld <jaspervanesveld@gmail.com>
2022-12-12 00:01:39 +0100

    Catch errors while demuxing
    Youtube sometimes throws 403 while loading data

--- src/demux/mod.ts
+++ src/demux/mod.ts
@@ -4,16 +4,20 @@ export async function* demux(source: AsyncIterable<Uint8Array>) {
   const worker = new Worker(workerUrl, {
     type: "module",
   });
-  for await (const value of source) {
-    const nextValue = new Promise<Uint8Array[]>((resolve) => {
-      worker.onmessage = (e) => {
-        resolve(e.data);
-      };
-    });
-    worker.postMessage(value);
-    for (const demuxed of await nextValue) {
-      yield demuxed;
+  try {
+    for await (const value of source) {
+      const nextValue = new Promise<Uint8Array[]>((resolve) => {
+        worker.onmessage = (e) => {
+          resolve(e.data);
+        };
+      });
+      worker.postMessage(value);
+      for (const demuxed of await nextValue) {
+        yield demuxed;
+      }
     }
+  } catch (error) {
+    console.error(error);
   }
   worker.terminate();
 }
