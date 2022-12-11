71f03ae636c2af7cd1bf9472c8765e1a56909905
Jasper van Esveld <jaspervanesveld@gmail.com>
2022-12-11 23:55:24 +0100

    Prevent crash if youtube search fails

--- src/audio-source/youtube.ts
+++ src/audio-source/youtube.ts
@@ -24,15 +24,20 @@ export async function getYoutubeSources(...queries: string[]) {
 }
 
 export async function getYoutubeSource(query: string) {
-  const results = await YouTube.search(query, { limit: 1, type: "video" });
-  if (results.length > 0) {
-    const { id, title } = results[0];
-    return createAudioSource(title!, async () => {
-      const info = await ytdl.getInfo(id!);
-      const audio = await ytdl.downloadFromInfo(info, {
-        filter: supportedFormatFilter,
+  try {
+    const results = await YouTube.search(query, { limit: 1, type: "video" });
+    if (results.length > 0) {
+      const { id, title } = results[0];
+      return createAudioSource(title!, async () => {
+        const info = await ytdl.getInfo(id!);
+        const audio = await ytdl.downloadFromInfo(info, {
+          filter: supportedFormatFilter,
+        });
+        return bufferIter(demux(audio));
       });
-      return bufferIter(demux(audio));
-    });
+    }
+  } catch (error) {
+    console.error(error);
+    return undefined;
   }
 }
