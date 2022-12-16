dc6b43c0af2adb81d5fb56c1df923d0543aa28d5
JasperVanEsveld <jaspervanesveld@gmail.com>
2022-12-16 15:54:50 +0100

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -2,6 +2,6 @@ export * from "https://deno.land/x/discordeno@17.1.0/mod.ts";
 export * from "https://deno.land/x/discordeno@17.1.0/plugins/cache/mod.ts";
 export * as opus from "https://unpkg.com/@evan/wasm@0.0.95/target/opus/deno.js";
 export * from "https://unpkg.com/@evan/wasm@0.0.95/target/nacl/deno.js";
-export { ytDownload } from "https://deno.land/x/yt_download@1.0/mod.ts";
+export { ytDownload } from "https://deno.land/x/yt_download@1.1/mod.ts";
 export type { VideoFormat } from "https://deno.land/x/ytdl_core@v0.1.0/src/types.ts";
 export { default as YouTube } from "https://deno.land/x/youtube_sr@v4.1.17/mod.ts";
