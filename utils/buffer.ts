bc29a5bf606cdaad7f16458e2cffebee60c74ef1
Jasper van Esveld <jaspervanesveld@gmail.com>
2023-07-06 13:53:26 +0200

    Fix not properly ending audio stream

--- utils/buffer.ts
+++ utils/buffer.ts
@@ -1,19 +1,27 @@
 import { EventSource } from "./mod.ts";
 
-export function buffered<T>(iterator: AsyncIterableIterator<T>) {
+export function buffered<T>(iterator: AsyncIterable<T>) {
   const source = new EventSource<T>();
-  toCallback(iterator, (value) => {
-    source.trigger(value);
-    return value;
-  });
+  toCallback(
+    iterator,
+    (value) => {
+      source.trigger(value);
+      return value;
+    },
+    () => {
+      source.disconnect();
+    }
+  );
   return source.iter();
 }
 
 async function toCallback<T>(
-  iterator: AsyncIterableIterator<T>,
-  callback: (value: T) => unknown
+  iterator: AsyncIterable<T>,
+  callback: (value: T) => void,
+  done: () => void
 ) {
   for await (const value of iterator) {
     callback(value);
   }
+  done();
 }
