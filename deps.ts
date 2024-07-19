b29f257b4d031e7d2587ba14e304c243f1e8b9a7
Jasper van Esveld <jaspervanesveld@gmail.com>
2024-07-19 17:26:04 +0200

    Update yt download dependencies to 1.9

--- deps.ts
+++ deps.ts
@@ -2,5 +2,5 @@ export * from "https://deno.land/x/discordeno@17.1.0/mod.ts";
 export * from "https://deno.land/x/discordeno@17.1.0/plugins/cache/mod.ts";
 export * as opus from "https://unpkg.com/@evan/wasm@0.0.95/target/opus/deno.js";
 export * from "https://unpkg.com/@evan/wasm@0.0.95/target/nacl/deno.js";
-export { ytDownload } from "https://deno.land/x/yt_download@1.8/mod.ts";
+export { ytDownload } from "https://deno.land/x/yt_download@1.9/mod.ts";
 export { default as YouTube } from "https://deno.land/x/youtube_sr@v4.1.17/mod.ts";
