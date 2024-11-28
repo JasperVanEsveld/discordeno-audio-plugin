31f5dc609df154be7229753802facb04354b1d36
Jasper van Esveld <jaspervanesveld@gmail.com>
2024-11-28 21:12:32 +0100

    Update discordeno to v20

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export * from "npm:@discordeno/bot@19.0.0";
+export * from "npm:@discordeno/bot@20.0.0";
 export * as opus from "https://unpkg.com/@evan/wasm@0.0.95/target/opus/deno.js";
 export * from "https://unpkg.com/@evan/wasm@0.0.95/target/nacl/deno.js";
 export {
